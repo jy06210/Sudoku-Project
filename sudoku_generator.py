d4da45e6a5c3a6a049596cb7d094c563b6e79c89
diya <pateld6@ufl.edu>
2024-11-19 16:23:46 -0500

    Revert "jasmine"
    This reverts commit e6e2b1903ef3d3e4f4205e2b51f1a3471534c9e2.

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -28,7 +28,6 @@ class SudokuGenerator():
         row_length = 9
         self.row_length = row_length
         self.removed_cells = removed_cells
-        self.removed_cells_list = []
 
         self.board =[[],[],[],[],[],[],[],[],[]]
         self.box_length = (row_length) ** .5
@@ -239,7 +238,6 @@ class SudokuGenerator():
                     continue
                 else:
                     break
-            self.removed_cells_list.append((x,y))
             self.board[x][y] = 0
 
 '''
